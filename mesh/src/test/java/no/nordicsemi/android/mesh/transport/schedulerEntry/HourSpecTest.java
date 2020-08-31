daa431396d0b421bf86bd0f28eb33d6ec8cffac4
Christof Wolke <cwolke@slashwhy.de>
2020-08-31 15:20:21 +0200

    Test fix

--- mesh/src/test/java/no/nordicsemi/android/mesh/transport/schedulerEntry/HourSpecTest.java
+++ mesh/src/test/java/no/nordicsemi/android/mesh/transport/schedulerEntry/HourSpecTest.java
@@ -12,12 +12,12 @@ public class HourSpecTest {
     public void should_have_the_correct_values() {
         assertEquals(0x18, ScheduleEntry.Hour.Any.getValue());
         assertEquals(0x19, ScheduleEntry.Hour.Random.getValue());
-        assertEquals(0xE, ScheduleEntry.Hour.Specific(14).getValue());
+        assertEquals(0xE, ScheduleEntry.Hour.Value(14).getValue());
     }
 
     @Test
     public void create_specific_hour_value_max_value_is_23() {
-        assertEquals(0x17, ScheduleEntry.Hour.Specific(23).getValue());
-        assertEquals(0x17, ScheduleEntry.Hour.Specific(30).getValue());
+        assertEquals(0x17, ScheduleEntry.Hour.Value(23).getValue());
+        assertEquals(0x17, ScheduleEntry.Hour.Value(30).getValue());
     }
 }
